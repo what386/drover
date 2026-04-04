d28b8bbd46d93a4a1ed7f81e746696d2dd2e38ef
bmorin <robot0324@gmail.com>
2026-04-04 19:19:33 -0400

    entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod cli;
+mod config;
+mod ollama;
+mod run;
+
+use cli::Cli;
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().skip(1);
+
+    if let Err(err) = Cli::parse(args).and_then(|cli| cli.run()) {
+        #[cfg(debug_assertions)]
+        {
+            eprintln!("{:?}", err);
+        }
+
+        #[cfg(not(debug_assertions))]
+        {
+            eprintln!(
+                "{}",
+                err.chain()
+                    .map(|e| e.to_string())
+                    .collect::<Vec<_>>()
+                    .join("\n")
+            );
+        }
+
+        std::process::exit(1);
+    }
 }
